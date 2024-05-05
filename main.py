ec5ed81f73724fb3115bba19d26b99875ef1e6dd
NoxKray <165674366+NoxKray@users.noreply.github.com>
2024-05-05 18:38:32 -0300

    main.py

--- main.py
+++ main.py
@@ -191,3 +191,23 @@ if __name__ == "__main__":
 
     args, _ = parser.parse_known_args()
     install_dependents(skip=args.skip_pip)
+
+    from fooocusapi.args import args
+
+    if prepare_environments(args):
+        sys.argv = [sys.argv[0]]
+
+        # Load pipeline in new thread
+        preload_pipeline_thread = Thread(target=preload_pipeline, daemon=True)
+        preload_pipeline_thread.start()
+
+        # Start task schedule thread
+        from fooocusapi.worker import task_schedule_loop
+
+        task_schedule_thread = Thread(target=task_schedule_loop, daemon=True)
+        task_schedule_thread.start()
+
+        # Start api server
+        from fooocusapi.api import start_app
+
+        start_app(args)
